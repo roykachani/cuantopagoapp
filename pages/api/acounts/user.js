3a2bacec7fd6d61a28c122f8a22eeea0dcee2e76
Roy Kachani <roy.kachani@gmail.com>
2022-09-05 17:05:41 -0300

    user api route

---
+++ pages/api/acounts/user.js
@@ -0,0 +1,46 @@
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from '../../../prisma/user';
+
+export default async function handleAcounts(req, res) {
+  try {
+    switch (req.method) {
+      case 'GET': {
+        if (req.query.id) {
+          const user = await getUser(req.query.id);
+
+          return res.status(200).json(user);
+        } else {
+          const users = await getAllUser();
+
+          return res.status(200).json(users);
+        }
+      }
+      case 'POST': {
+        const data = req.body;
+        console.log(data);
+        const user = await createUser(req.body);
+
+        return res.json(user);
+      }
+      case 'PUT': {
+        const { id, ...updateData } = req.body;
+        const user = await updateUser(id, ...updateData);
+
+        return res.json(user);
+      }
+      case 'DELETE': {
+        const { id } = req.body;
+        const user = await deleteUser(id);
+
+        return res.json(user);
+      }
+    }
+  } catch (e) {
+    return res.status(500).json({ ...e, message: e.message });
+  }
+}
