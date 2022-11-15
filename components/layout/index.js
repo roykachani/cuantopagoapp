81b1143d1dfd876546a6f845b941eb18202df259
Roy Kachani <roy.kachani@gmail.com>
2022-11-15 16:40:30 -0300

    layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
   return (
-    <main className="font-nunito my-0 mx-auto flex flex-col h-screen bg-gradient-to-tr from-yellow-100 to-amber-50 ">
+    <main className="font-nunito my-0 mx-auto flex flex-col h-screen bg-white ">
       {children}
     </main>
   );
